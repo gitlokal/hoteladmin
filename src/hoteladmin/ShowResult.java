ed383eb89dfc0c73efe751950717bd683abc3a18
gitlokal <gitlokal@gmail.com>
2015-09-09 13:49:49 +0200

    pobierania z bazy dziala

--- src/hoteladmin/ShowResult.java
+++ src/hoteladmin/ShowResult.java
@@ -5,31 +5,60 @@
  */
 package hoteladmin;
 import javax.swing.JTable;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author lokal
  */
-public class ShowResult extends GuestForm {
-      String[] columnNames = {"First Name",
-                        "Last Name",
-                        "address",
-                        "city",
-                        "phone"};
-    
+public class ShowResult extends JPanel {
+      String[] columnNames = {"First Name","Last Name","address", "city", "phone"};
+            private SearchGuest search;
             private Object[][] data;
-            JTable table;
-            public ShowResult(String name, String lastName, String address, String city, String phone){
-                super("tralal");
-//                this.nameField.setText(name);
-//                this.lastNameField.setText(lastName);
-//                this.streetField.setText(address);
-//                this.phoneField.setText(phone);
-//                this.cityField.setText(city);
+            private JTable table=new JTable();
+            private DefaultTableModel model;
+ 
+            public ShowResult(){
+
                 
-                data =  new Object[][]{ {name, lastName,  address,  city,  phone}};
+            }
+            public ShowResult(String name, String lastName, String address, String city, String phone){
+ 
+                data =  new Object[][]{ { name, lastName,  address,  city,  phone } };
                 table = new JTable(data, columnNames);
+                table.setRowHeight(25);
+                this.setLayout(new BorderLayout());
+                this.add(table.getTableHeader(), BorderLayout.PAGE_START);
                 add(table);
             }
+
+    public void setSearch(SearchGuest search1) {
+       
+        this.search = search1;
+        data = new Object[][]{ { search.getName(), search.getLastName(), search.getStreet(), search.getCity(), search.getPhone() } };
+        model = new DefaultTableModel(data, columnNames){
+            
+                @Override
+                public boolean isCellEditable(int row, int column){
+                     return false;
+                }
+        };
+        
+        table = new JTable(model);
+        table.setModel(model);
+        table.setRowHeight(25);
+        
+        this.setLayout(new BorderLayout());
+        this.add(table.getTableHeader(), BorderLayout.PAGE_START); 
+        
+         add(table);
+         table.getTableHeader().setReorderingAllowed(false);
+         //table.setRowSelectionAllowed(false); needed?
+         table.setCellSelectionEnabled(true);
+
+    }
               
-  
+            
 }
